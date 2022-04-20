9ffd64d63cb9d1013e553e9f14a3cf2c57ae32cc
SaillyAlexandre <87168625+SaillyAlexandre@users.noreply.github.com>
2022-04-20 15:10:00 +0200

    iuiu
    iuiui

--- js/index.js
+++ js/index.js
@@ -9,8 +9,8 @@ $ = function(id) {
       $(id).style.display ='none';
   }
   
-  let slide = new Array("../img/276968519_631563404585707_3161485913316059396_n.png", "../img/terretrois.gif", "../img/20220411_142755.jpg", "../img/20220411_143015.jpg",
- "../img/20220411_142623.jpg", "../img/20220411_143651.jpg");
+  let slide = new Array("../html/img/port1.jpg", "../html/img/port2.jpg", "../html/img/port3.jpg", "../html/img/port4.jpg","../html/img/port5.jpg","../html/img/port6.jpg","../html/img/port7.jpg","../html/img/port8.jpg","../html/img/port9.jpg","../html/img/port10.jpg");
+ 
 let numero = 0;
 
 function ChangeSlide(sens) {
